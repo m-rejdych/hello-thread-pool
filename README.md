3c28179b8ed8f17bb356525584ce8fc4974f0f9b
m-rejdych <rejdych.m.r@gmail.com>
2022-03-17 22:02:12 +0100

    Finished project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello thread pool
+
+A simple, multi-threaded web server using thread pool pattern, made as a final project in [The Rust Programming Language](https://doc.rust-lang.org/book/) book.
---
+++ src/lib.rs
@@ -0,0 +1,102 @@
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)();
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job executing", id);
+                    job.call_box();
+                },
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                },
+            }
+        });
+        let thread = Some(thread);
+
+        Worker { id, thread }
+    }
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (tx, rx) = mpsc::channel();
+
+        let rx = Arc::new(Mutex::new(rx));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&rx)));
+        }
+
+        ThreadPool {
+            workers,
+            sender: tx,
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Message::NewJob(Box::new(f));
+        self.sender.send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in self.workers.iter() {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        println!("Shutting down all workers!");
+
+        for worker in &mut self.workers {
+            println!("Cleaning up worker: {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
